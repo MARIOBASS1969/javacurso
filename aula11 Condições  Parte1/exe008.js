875be2464f0da4a217790c9ef65be0f48f4a2cde
Mario Roberto de Oliveira <94222042+MARIOBASS1969@users.noreply.github.com>
2021-11-18 11:57:43 -0300

    Initial commit

---
+++ aula11 Condições  Parte1/exe008.js
@@ -0,0 +1,7 @@
+var vel = 75
+console.log(`a velocidade é ${vel}Km/h`)
+if (vel > 60) {  // condição simples
+    console.log(`Você Ultrapassou a velocidade permitida. MULTADO!`)
+}
+
+console.log(`Dirija sempre usando o cinto de segurança`)
---
+++ aula13 Repetições1/ambiente.js
@@ -0,0 +1,23 @@
+// while    estrutura de repetição com teste lógico no inicio
+// Do while estrutura de repetição com teste lógico no final
+
+var c = 1
+do {
+   console.log(`passo ${c}`)
+   c++ // c = c + 1
+} while (c <= 6) 
+
+//var c = 1
+//while (c <= 6) {
+//    console.log(`passo ${c}`)
+//    c++ // c = c + 1
+//}
+
+/*
+console.log('Tudo Bem?')
+console.log('Tudo Bem?')
+console.log('Tudo Bem?')
+console.log('Tudo Bem?')
+console.log('Tudo Bem?')
+console.log('Tudo Bem?')
+*/
